53dd03765b1816b95562c60e55235d088cd2f69c
enextus <enextus@gmail.com>
2020-11-03 14:06:22 +0100

    overload Typetester

---
+++ src/lesson01/TypeTester.java
@@ -0,0 +1,23 @@
+package lesson01;
+
+public class TypeTester {
+    void printType(byte x) {
+        System.out.println(x + " is an byte");
+    }
+
+    void printType(int x) {
+        System.out.println(x + " is an int");
+    }
+
+    void printType(float x) {
+        System.out.println(x + " is an float");
+    }
+
+    void printType(double x) {
+        System.out.println(x + " is an double");
+    }
+
+    void printType(char x) {
+        System.out.println(x + " is an char");
+    }
+}
