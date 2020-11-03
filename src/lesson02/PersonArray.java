7c5d093ca2f085edf2b33419c162e912a5307990
enextus <enextus@gmail.com>
2020-11-03 14:20:22 +0100

    sort Person temp obj

--- src/lesson02/PersonArray.java
+++ src/lesson02/PersonArray.java
@@ -12,10 +12,10 @@ public class PersonArray {
         Person p2 = new Person("Caligula", 25, 170);
         Person p3 = new Person("Macron", 42, 180);
         Person p4 = new Person("Trump", 72, 150);
-        Person p5 = new Person("Biden", 76, 130);
+        Person p5 = new Person("Biden", 176, 130);
         Person p6 = new Person("Ivan", 18, 170);
         Person p7 = new Person("Uladzimir", 44, 156);
-        Person p8 = new Person("Merkel", 70, 199);
+        Person p8 = new Person("Merkel", 41, 199);
 
         Person[] arrP = new Person[]{p1, p2, p3, p4, p5, p6, p7, p8};
 
@@ -24,17 +24,15 @@ public class PersonArray {
 
     public static Person[] sort(Person[] arrP) {
 
-        Person temp;
+        Person obj;
         for (int i = 0; i < arrP.length - 1; i++) {
 
             if (arrP[i].weight < arrP[i + 1].weight) continue;
 
-            temp = arrP[i];
-
-            System.out.println("temp : " + temp);
+            obj = arrP[i];
 
             arrP[i] = arrP[i + 1];
-            arrP[i + 1] = temp;
+            arrP[i + 1] = obj;
 
             sort(arrP);
         }
