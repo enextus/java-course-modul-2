f3a61a4cb0996164b02b5bbbc1c76ba8af72d997
enextus <enextus@gmail.com>
2020-11-03 14:11:50 +0100

    sort Person arr

--- src/lesson02/PersonArray.java
+++ src/lesson02/PersonArray.java
@@ -7,13 +7,14 @@ public class PersonArray {
      */
 
     public static void main(String[] args) {
+
         Person p1 = new Person("Anton", 90, 190);
         Person p2 = new Person("Caligula", 25, 170);
         Person p3 = new Person("Macron", 42, 180);
         Person p4 = new Person("Trump", 72, 150);
         Person p5 = new Person("Biden", 76, 130);
         Person p6 = new Person("Ivan", 18, 170);
-        Person p7 = new Person("Vova", 44, 156);
+        Person p7 = new Person("Uladzimir", 44, 156);
         Person p8 = new Person("Merkel", 70, 199);
 
         Person[] arrP = new Person[]{p1, p2, p3, p4, p5, p6, p7, p8};
