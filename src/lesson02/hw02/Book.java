50702bfad7adc245f0cb4cfd965328be5749e63e
enextus <enextus@gmail.com>
2020-11-04 09:48:43 +0100

    Book class upd

--- src/lesson02/hw02/Book.java
+++ src/lesson02/hw02/Book.java
@@ -4,13 +4,33 @@ public class Book {
 
     /**
      * Class Book
+     *
+     * Test:
+     * The ID is: 1
+     * The author is: Leo Tolstoy
+     * The height is: War and Peace
+     * The BMI is: 1869
+     *
+     * The ID is: 2
+     * The author is: Jane Austen
+     * The height is: Pride and Prejudice
+     * The BMI is: 1813
+     *
+     * The ID is: 3
+     * The author is: Dante Alighieri
+     * The height is: Divine Comedy
+     * The BMI is: 1321
+     *
+     * The ID is: 4
+     * The author is: Joshua Bloch
+     * The height is: Effective Java
+     * The BMI is: 2001
      */
 
     int id;
     String author;
     String title;
     int year;
-
 
     public Book(int bookId, String bookAuthor, String bookTitle, int bookYear) {
         id = bookId;
@@ -27,7 +47,6 @@ public class Book {
     }
 }
 
-
 class BookTest {
     public static void main(String[] args) {
         Book bookOne = new Book(1, "Leo Tolstoy", "War and Peace", 1869);
@@ -35,11 +54,15 @@ class BookTest {
         Book bookThree = new Book(3, "Dante Alighieri", "Divine Comedy", 1321);
         Book bookFour = new Book(4, "Joshua Bloch", "Effective Java", 2001);
 
-
         bookOne.displayInfo();
-        bookTwo.displayInfo();
-        bookThree.displayInfo();
-        bookFour.displayInfo();
         System.out.println();
+
+        bookTwo.displayInfo();
+        System.out.println();
+
+        bookThree.displayInfo();
+        System.out.println();
+
+        bookFour.displayInfo();
     }
 }
