7bd3b59361aacc3c97347705ff9b43b4491ac86c
enextus <enextus@gmail.com>
2020-11-04 09:23:34 +0100

    least common multiple upd

--- src/lesson02/hw02/Lcm.java
+++ src/lesson02/hw02/Lcm.java
@@ -3,7 +3,15 @@ package lesson02.hw02;
 public class Lcm {
 
     /**
-     * LCM
+     * In arithmetic and number theory, the least common multiple, lowest common multiple, or smallest common multiple
+     * of two integers a and b, usually denoted by lcm(a, b), is the smallest positive integer that is divisible by both a and b.
+     * Since division of integers by zero is undefined, this definition has meaning only if a and b are both different from zero.
+     * However, some authors define lcm(a,0) as 0 for all a, which is the result of taking the lcm to be the least upper bound in the lattice of divisibility.
+     *
+     * The lcm is the "lowest common denominator" (lcd) that can be used before fractions can be added, subtracted or compared.
+     * The lcm of more than two integers is also well-defined: it is the smallest positive integer that is divisible by each of them.
+     *
+     *
      */
 
     public static void main(String[] args) {
@@ -16,7 +24,7 @@ public class Lcm {
 
     public static int getLcm(int a, int b) {
 
-        return -1;
+        return a * b / getGcd(b, a % b);
     }
 
     public static int getGcd(int a, int b) {
