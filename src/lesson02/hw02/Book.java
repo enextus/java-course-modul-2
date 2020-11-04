836a847831c824e74e7405755c895e091424ee52
enextus <enextus@gmail.com>
2020-11-04 09:42:50 +0100

    Book class upd

--- src/lesson02/hw02/Book.java
+++ src/lesson02/hw02/Book.java
@@ -31,6 +31,10 @@ public class Book {
 class BookTest {
     public static void main(String[] args) {
         Book bookOne = new Book(1, "Leo Tolstoy", "War and Peace", 1869);
+        Book bookOne = new Book(1, "Jane Austen", "Pride and Prejudice", 1813);
+        Book bookOne = new Book(1, "Leo Tolstoy", "War and Peace", 1869);
+        Book bookOne = new Book(1, "Leo Tolstoy", "War and Peace", 1869);
+
 
         bookOne.displayInfo();
     }
