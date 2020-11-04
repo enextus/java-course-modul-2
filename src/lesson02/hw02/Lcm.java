d7a167d57d3b2a90802730fef1ecb21bd163bb29
enextus <enextus@gmail.com>
2020-11-04 09:20:25 +0100

    least common multiple

---
+++ src/lesson02/hw02/Lcm.java
@@ -0,0 +1,21 @@
+package lesson02.hw02;
+
+public class Lcm {
+
+    /**
+     * LCM
+     */
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 100;
+
+        System.out.println(getLcm(a, b));
+    }
+
+    public static int getLcm(int a, int b) {
+
+        return -1;
+    }
+}
