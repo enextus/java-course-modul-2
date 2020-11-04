5833acc1035449932ba18aab89cfc5890830d74c
enextus <enextus@gmail.com>
2020-11-04 09:34:09 +0100

    Book class upd

--- src/lesson02/hw02/Book.java
+++ src/lesson02/hw02/Book.java
@@ -1,4 +1,21 @@
 package lesson02.hw02;
 
 public class Book {
+
+    /**
+     * Class Book
+     */
+
+    int id;
+    String author;
+    String title;
+    int year;
+
+
+    public Book(int ID, String bookAuthor, String bookTitle, int bookYear) {
+        id = ID;
+        author = bookAuthor;
+        title = bookTitle;
+        year = bookYear;
+    }
 }
