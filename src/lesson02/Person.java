a231f977e475bb0023f23861164453505b13cae3
enextus <enextus@gmail.com>
2020-11-09 07:40:25 +0100

    Insertion Sort upd

--- src/lesson02/Person.java
+++ src/lesson02/Person.java
@@ -4,26 +4,22 @@ package lesson02;
  * Class Person
  */
 public class Person {
-
     public String name;
     public double weight;
     public double height;
     public double bmi;
 
     public Person(String username, double userWeight, double userHeight) {
-
         name = username;
         weight = userWeight;
         height = userHeight;
     }
 
     public void findBMI() {
-
         bmi = weight / ((height) * (height) / 10000);
     }
 
     public void displayInfo() {
-
         System.out.println("The name is: " + name);
         System.out.println("The weight is: " + weight);
         System.out.println("The height is: " + height);
