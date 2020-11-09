eb659a3651c037d8645f302f646a7e321b0af75b
enextus <enextus@gmail.com>
2020-11-09 07:55:00 +0100

    upd

--- src/lesson04/hw04/PersonArraySortInsertion.java
+++ src/lesson04/hw04/PersonArraySortInsertion.java
@@ -3,22 +3,20 @@ package lesson04.hw04;
 import lesson02.Person;
 
 /**
- * Person Array sort via recursive bubble sort
+ * Person Array sort via recursive insertion sort
  */
 public class PersonArraySortInsertion {
-
-
     public static void main(String[] args) {
 
         Person p1 = new Person("Anton", 90, 190);
         Person p2 = new Person("Caligula", 25, 170);
         Person p3 = new Person("Macron", 42, 180);
         Person p4 = new Person("Trump", 72, 150);
-        Person p5 = new Person("Biden", 176, 130);
+        Person p5 = new Person("Biden", 76, 130);
         Person p6 = new Person("Ivan", 18, 170);
         Person p7 = new Person("Uladzimir", 44, 156);
         Person p8 = new Person("Merkel", 41, 199);
-        Person p9 = new Person("Burvat", 38, 174);
+        Person p9 = new Person("Erdogan", 55, 174);
 
         Person[] arrP = new Person[]{p1, p2, p3, p4, p5, p6, p7, p8, p9};
 
@@ -45,6 +43,6 @@ public class PersonArraySortInsertion {
 
     public static void printArr(Person[] arr) {
 
-        for (Person element : arr) System.out.println(element.weight + " ");
+        for (Person element : arr) System.out.println("Person name: " + element.name + ", weight: " + element.weight);
     }
 }
