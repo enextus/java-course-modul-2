86b0a791c5a9056fa1ac9401a801478e06a8aea8
enextus <enextus@gmail.com>
2020-11-10 08:42:36 +0100

    upd

--- src/lesson05/uni/Lecturer.java
+++ src/lesson05/uni/Lecturer.java
@@ -1,11 +1,11 @@
 package lesson05.uni;
 
 
-public class Teacher extends Person {
+public class Lecturer extends Person {
     private String rank;
     private String degree;
 
-    public Teacher(String firstName, String secondName, int age, String uniID, String occupation, String constructor, String rank, String degree) {
+    public Lecturer(String firstName, String secondName, int age, String uniID, String occupation, String rank, String degree) {
         super(firstName, secondName, age, uniID, occupation);
         this.rank = rank;
         this.degree = degree;
