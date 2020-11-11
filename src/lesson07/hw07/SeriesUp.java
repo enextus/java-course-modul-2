c35f219e6371d7f700cc25ddc3b5b062e1b0d7ef
enextus <enextus@gmail.com>
2020-11-11 20:15:36 +0100

    seriesUp

--- src/lesson07/hw07/SeriesUp.java
+++ src/lesson07/hw07/SeriesUp.java
@@ -1,7 +1,5 @@
 package lesson07.hw07;
 
-import lesson05.uni.Student;
-
 /**
  * Given n>=0, create an array with the pattern {1, 1, 2, 1, 2, 3, .. 1, 2, 3 .. n} (spaces added to show the grouping).
  * Note that the length of the array will be 1 + 2 + 3 .. + n, which is known to sum to exactly n*(n + 1)/2.
@@ -19,34 +17,22 @@ public class SeriesUp {
     public static void main(String[] args) {
 
         printArr(seriesUp(2));
-        System.out.println();
-
         printArr(seriesUp(3));
-        System.out.println();
-
         printArr(seriesUp(4));
-        System.out.println();
     }
 
     private static int[] seriesUp(int i) {
 
         int arrayLength = i * (i + 1) / 2;
 
-        //System.out.println(arrayLength);
-
         int[] students = new int[arrayLength];
 
-        int counter = 0;
-        for (int j = 1; j <= i; j++) {  // kol-wo serij
-
-            for (int k = 1; k <= j; k++) { // sama serija
-
-                //System.out.print(k  + "(" + (counter)  + ")" + " ");
-                students[counter] = k;
-
-                counter++;
+        int index = 0;
+        for (int j = 1; j <= i; j++) {
+            for (int k = 1; k <= j; k++) {
+                students[index] = k;
+                index++;
             }
-            //System.out.print(" ");
         }
 
         return students;
@@ -54,5 +40,6 @@ public class SeriesUp {
 
     private static void printArr(int[] arr) {
         for (int element : arr) System.out.print(element + " ");
+        System.out.println();
     }
 }
