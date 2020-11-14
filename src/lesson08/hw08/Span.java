9c32cdcf8c54daa8bfb52a0385a5bd5abbf880cc
enextus <enextus@gmail.com>
2020-11-14 03:29:52 +0100

    span upd

--- src/lesson08/hw08/Span.java
+++ src/lesson08/hw08/Span.java
@@ -1,36 +1,45 @@
 package lesson08.hw08;
 
+/**
+ * Consider the leftmost and rightmost appearances of some value in an array.
+ * We'll say that the "span" is the number of elements between the two inclusive.
+ * A single value has a span of 1. Returns the largest span found in the given array.
+ * (Efficiency is not a priority.)
+ * <p>
+ * Expectation:
+ * maxSpan([1, 2, 1, 1, 3])         → 4
+ * maxSpan([1, 4, 2, 1, 4, 1, 4])   → 6
+ * maxSpan([1, 4, 2, 1, 4, 4, 4])   → 6
+ * Test:
+ */
 public class Span {
-
-    public static int [] arr;
-    /**
-     * Consider the leftmost and rightmost appearances of some value in an array.
-     * We'll say that the "span" is the number of elements between the two inclusive.
-     * A single value has a span of 1. Returns the largest span found in the given array.
-     * (Efficiency is not a priority.)
-     *
-     * Expectation:
-     * maxSpan([1, 2, 1, 1, 3]) → 4
-     * maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
-     * maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
-     * Test:
-     *
-     */
+    public static int[] arr;
 
     public static void main(String[] args) {
-
-        maxSpan();
-
-
-
-
+        System.out.println(maxSpan(1, 2, 1, 1, 3));
+        System.out.println(maxSpan(1, 4, 2, 1, 4, 1, 4));
+        System.out.println(maxSpan(1, 4, 2, 1, 4, 4, 4));
     }
 
-    private static void maxSpan() {
+    private static int maxSpan(int... args) {
+        arr = args;
+        int target;
+        int indexOfTarget;
+        int span;
+        int spanResult = 0;
 
+        for (int i = 0; i < arr.length; i++) {
+            target = arr[i];
+            indexOfTarget = findIndexOfTarget(target);
 
-        for (int i = 0; i < ; i++) {
-
+            span = indexOfTarget;
+            if (spanResult < span) spanResult = span;
         }
+        return spanResult;
+    }
+
+    public static int findIndexOfTarget(int target) {
+        for (int i = arr.length - 1; i > 0; i--) if (target == arr[i]) return i;
+        return -1;
     }
 }
