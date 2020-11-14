dd2c8b0375fd1801c96700c32f6a5ddd7c7f14b0
enextus <enextus@gmail.com>
2020-11-14 07:46:55 +0100

    LinearIn ref

--- src/lesson08/hw08/LinearIn.java
+++ src/lesson08/hw08/LinearIn.java
@@ -6,12 +6,12 @@ package lesson08.hw08;
  * Note:
  * The best solution makes only a single "linear" pass of both arrays,
  * taking advantage of the fact that both arrays are already in sorted order.
- * <p>
+ * *
  * Expectation:
  * linearIn([1, 2, 4, 6], [2, 4]) → true
  * linearIn([1, 2, 4, 6], [2, 3, 4]) → false
  * linearIn([1, 2, 4, 4, 6], [2, 4]) → true
- * <p>
+ * *
  * Test:
  */
 public class LinearIn {
@@ -29,22 +29,19 @@ public class LinearIn {
 
         for (int i = 0; i < arrInner.length; i++) {
             result = recursiveBinarySearch(arrOuter, arrOuter[0], arrOuter[arrInner.length - 1], arrInner[i]);
-            if (result == -1 ) return false;
+            if (result == -1) return false;
         }
         return true;
     }
 
     public static int recursiveBinarySearch(int arr[], int firstElement, int lastElement, int elementToSearch) {
-        if (lastElement >= firstElement) { // break
+        if (lastElement >= firstElement) {
             int mid = firstElement + (lastElement - firstElement) / 2;
 
-            if (arr[mid] == elementToSearch) // если средний элемент - целевой элемент, вернуть его индекс
-                return mid;
+            if (arr[mid] == elementToSearch) return mid;
+            if (arr[mid] > elementToSearch) return recursiveBinarySearch(arr, firstElement, mid - 1, elementToSearch);
 
-            if (arr[mid] > elementToSearch) // если средний элемент больше целевого - > вызываем метод рекурсивно по суженным данным
-                return recursiveBinarySearch(arr, firstElement, mid - 1, elementToSearch);
-
-            return recursiveBinarySearch(arr, mid + 1, lastElement, elementToSearch); // также, вызываем метод рекурсивно по суженным данным
+            return recursiveBinarySearch(arr, mid + 1, lastElement, elementToSearch);
         }
         return -1;
     }
