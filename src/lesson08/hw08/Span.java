e8c099c4fb93e35ebe7ea1aafef4728f4a42dab2
enextus <enextus@gmail.com>
2020-11-14 03:09:36 +0100

    span

---
+++ src/lesson08/hw08/Span.java
@@ -0,0 +1,17 @@
+package lesson08.hw08;
+
+public class Span {
+
+    /**
+     * Consider the leftmost and righmost appearances of some value in an array. We'll say that the "span" is the number of
+     * elements between the two inclusive. A single value has a span of 1. Returns the largest span found in the given array.
+     * (Efficiency is not a priority.)
+     *
+     * Expectation:
+     * maxSpan([1, 2, 1, 1, 3]) → 4
+     * maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
+     * maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
+     * Test:
+     *
+     */
+}
