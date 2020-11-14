68d13d2cf4e77e8dc285d4d214c1dd2a2455cbe0
enextus <enextus@gmail.com>
2020-11-14 03:12:26 +0100

    span

--- src/lesson08/hw08/Span.java
+++ src/lesson08/hw08/Span.java
@@ -2,9 +2,11 @@ package lesson08.hw08;
 
 public class Span {
 
+    public static int [] arr;
     /**
-     * Consider the leftmost and righmost appearances of some value in an array. We'll say that the "span" is the number of
-     * elements between the two inclusive. A single value has a span of 1. Returns the largest span found in the given array.
+     * Consider the leftmost and rightmost appearances of some value in an array.
+     * We'll say that the "span" is the number of elements between the two inclusive.
+     * A single value has a span of 1. Returns the largest span found in the given array.
      * (Efficiency is not a priority.)
      *
      * Expectation:
@@ -14,4 +16,21 @@ public class Span {
      * Test:
      *
      */
+
+    public static void main(String[] args) {
+
+        maxSpan();
+
+
+
+
+    }
+
+    private static void maxSpan() {
+
+
+        for (int i = 0; i < ; i++) {
+
+        }
+    }
 }
