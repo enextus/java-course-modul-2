6cab6e2897b188741c43c3033d8dd3fdcede3418
enextus <enextus@gmail.com>
2020-11-14 03:33:13 +0100

    span

--- src/lesson08/hw08/Span.java
+++ src/lesson08/hw08/Span.java
@@ -5,7 +5,7 @@ package lesson08.hw08;
  * We'll say that the "span" is the number of elements between the two inclusive.
  * A single value has a span of 1. Returns the largest span found in the given array.
  * (Efficiency is not a priority.)
- * <p>
+ * *
  * Expectation:
  * maxSpan([1, 2, 1, 1, 3])         → 4
  * maxSpan([1, 4, 2, 1, 4, 1, 4])   → 6
