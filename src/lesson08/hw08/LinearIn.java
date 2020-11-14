c57214e84ef2d23769e1a25441da3587b61cc187
enextus <enextus@gmail.com>
2020-11-14 07:24:51 +0100

    LinearIn ref

--- src/lesson08/hw08/LinearIn.java
+++ src/lesson08/hw08/LinearIn.java
@@ -3,6 +3,7 @@ package lesson08.hw08;
 /**
  * Given two arrays of ints sorted in increasing order, outer and inner,
  * return true if all of the numbers in inner appear in outer.
+ * Note:
  * The best solution makes only a single "linear" pass of both arrays,
  * taking advantage of the fact that both arrays are already in sorted order.
  *
