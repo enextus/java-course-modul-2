1874c6470da54b462128394f01fd0b1659faf405
enextus <enextus@gmail.com>
2020-11-14 04:05:00 +0100

    sums ref

--- src/lesson08/hw08/CompareSums.java
+++ src/lesson08/hw08/CompareSums.java
@@ -1,7 +1,8 @@
 package lesson08.hw08;
 
 /**
- * Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side is equal
+ * Given a non-empty array, return true if there is a place to split the array
+ * so that the sum of the numbers on one side is equal
  * to the sum of the numbers on the other side.
  * Expectation:
  * canBalance([1, 1, 1, 2, 1]) → true
@@ -14,22 +15,21 @@ public class CompareSums {
 
     public static void main(String[] args) {
         canBalance(1, 1, 1, 2, 1);
-        canBalance(2, 1, 1, 2, 1);
-        canBalance(10, 10);
+/*        canBalance(2, 1, 1, 2, 1);
+        canBalance(10, 10);*/
     }
 
     private static boolean canBalance(int... args) {
         arr = args;
 
         for (int i = 0; i < arr.length - 1; i++) {
-            System.out.println(sumToEnd(i));
+            System.out.println("sumFromBegin: " + sumFromBegin(arr[i]));
         }
-
 
         return false;
     }
 
-    private static int sumToEnd(int i) {
+    private static int sumFromBegin(int i) {
         int sum = 0;
 
         for (int j = 0; j < i; j++) {
@@ -38,7 +38,7 @@ public class CompareSums {
         return sum;
     }
 
-    private static int sumFromBegin() {
+    private static int sumToEnd() {
 
         return -1;
     }
