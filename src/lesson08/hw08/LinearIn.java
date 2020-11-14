347bf42b04d079e00e70cfe0d586ced77670d28a
enextus <enextus@gmail.com>
2020-11-14 07:23:44 +0100

    LinearIn ref

--- src/lesson08/hw08/LinearIn.java
+++ src/lesson08/hw08/LinearIn.java
@@ -25,7 +25,16 @@ public class LinearIn {
     }
 
     public static void linearIn(int[] arrOuter, int[] arrInner){
-        System.out.println(arrOuter);
-        System.out.println(arrInner);
+        printArr(arrOuter);
+        printArr(arrInner);
+    }
+
+    private static void printArr(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
     }
 }
