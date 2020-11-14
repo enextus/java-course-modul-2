9359cd919dd0ef515a77442fcd339c0b9dbfad23
enextus <enextus@gmail.com>
2020-11-14 03:49:50 +0100

    sums

--- src/lesson08/hw08/CompareSums.java
+++ src/lesson08/hw08/CompareSums.java
@@ -1,7 +1,26 @@
 package lesson08.hw08;
 
 /**
- *
+ * Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side is equal
+ * to the sum of the numbers on the other side.
+ * Expectation:
+ * canBalance([1, 1, 1, 2, 1]) → true
+ * canBalance([2, 1, 1, 2, 1]) → false
+ * canBalance([10, 10]) → true
+ * Test:
  */
 public class CompareSums {
+    int[] arr;
+
+    public static void main(String[] args) {
+        canBalance(1, 1, 1, 2, 1);
+        canBalance(2, 1, 1, 2, 1);
+        canBalance(10, 10);
+    }
+
+    private static boolean canBalance(int... args) {
+        arr = args;
+
+        return false;
+    }
 }
