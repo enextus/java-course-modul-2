2467fa97dabca356b0be353d79a6a34663589570
enextus <enextus@gmail.com>
2020-11-14 07:42:18 +0100

    LinearIn ref

--- src/lesson08/hw08/LinearIn.java
+++ src/lesson08/hw08/LinearIn.java
@@ -48,13 +48,4 @@ public class LinearIn {
         }
         return -1;
     }
-
-    private static void printArr(int[] arr) {
-        System.out.print("[");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-            if (i < arr.length - 1) System.out.print(", ");
-        }
-        System.out.println("]");
-    }
 }
