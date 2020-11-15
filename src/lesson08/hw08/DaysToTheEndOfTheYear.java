a74902c9e5cd6b6318c08db92cfd608f8706ebec
enextus <enextus@gmail.com>
2020-11-15 17:16:39 +0100

    DaysToTheEndOfTheYear ref

--- src/lesson08/hw08/DaysToTheEndOfTheYear.java
+++ src/lesson08/hw08/DaysToTheEndOfTheYear.java
@@ -4,34 +4,41 @@ import java.util.Scanner;
 
 /**
  * Program for determining the day number from the beginning of the year.
+ * 1804 1904 2004
+ * 1808 1908 2008
+ * 1812 1912 2012
  */
 public class DaysToTheEndOfTheYear {
 
-    static Scanner sc = new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
+    public static int leapDay = 0;
+    public static int[] numberOfMonth = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static int result;
 
     public static void main(String[] args) {
 
-        System.out.println("Please enter the year for the leap year test: ");
+        System.out.println("Please enter the year: ");
         int year = sc.nextInt();
 
+        System.out.println("Please enter the month: ");
+        int month = sc.nextInt();
+
+        System.out.println("Please enter the day: ");
+        int day = sc.nextInt();
+
         System.out.print("Year " + year);
+        System.out.print("Month " + year);
+        System.out.print("Day " + year);
 
 
+        if (checkLeapYear(year)) leapDay = 1;
 
+        result = 0;
 
-
-
-        System.out.print("Year " + year);
-
-        if (checkLeapYear(year) && checkMultiplicityOfFour(year)) System.out.println(" satisfies the condition of the specification.");
-        else System.out.println(" not satisfies the condition of the specification.");
     }
 
     private static boolean checkLeapYear(int a) {
         return ((a % 4) == 0 && a % 100 != 0) || (a % 400) == 0;
     }
 
-    private static boolean checkMultiplicityOfFour(int a) {
-        return (a % 4) == 0;
-    }
 }
