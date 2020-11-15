043f2914708fb3321721f1fd111b0f742d7191fe
enextus <enextus@gmail.com>
2020-11-15 19:34:58 +0100

    DateToEpoch ref

--- src/lesson08/hw08/DateToEpochConverter.java
+++ src/lesson08/hw08/DateToEpochConverter.java
@@ -1,4 +1,27 @@
 package lesson08.hw08;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+/**
+ * A calculator that takes a date from the user and returns a Unix time.
+ */
 public class DateToEpochConverter {
+
+
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        Timestamp timestamp = Timestamp.valueOf(now);
+
+        System.out.println(now);
+
+        LocalDateTime time = LocalDateTime.now();
+        ZoneId zoneId = ZoneId.systemDefault(); // or: ZoneId.of("Europe/Berlin");
+        long epoch = time.atZone(zoneId).toEpochSecond();
+
+        System.out.println("epoch: " + epoch);
+
+
+    }
 }
