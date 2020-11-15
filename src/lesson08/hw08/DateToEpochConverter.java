3282d40227b4b91e8d7c29f92ff03941a6bc4e0a
enextus <enextus@gmail.com>
2020-11-15 19:44:45 +0100

    DateToEpoch ref

--- src/lesson08/hw08/DateToEpochConverter.java
+++ src/lesson08/hw08/DateToEpochConverter.java
@@ -2,12 +2,18 @@ package lesson08.hw08;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Scanner;
 
 /**
  * A calculator that takes a date from the user and returns a Unix time.
  */
 public class DateToEpochConverter {
     public static void main(String[] args) {
+        System.out.print("Please enter date of birth in YYYY-MM-DD: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+
         LocalDateTime time = LocalDateTime.now();
         System.out.println("Now is: " + time);
 
