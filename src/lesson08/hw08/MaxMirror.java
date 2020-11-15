a20a72bb03076a2854e9740496c761642e037a19
enextus <enextus@gmail.com>
2020-11-15 09:16:45 +0100

    maxMirror ref

--- src/lesson08/hw08/MaxMirror.java
+++ src/lesson08/hw08/MaxMirror.java
@@ -2,6 +2,31 @@ package lesson08.hw08;
 
 /**
  * class maxMirror
+ * We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array,
+ * the same group appears in reverse order.
+ * For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part).
+ * Return the size of the largest mirror section found in the given array.
+ * <p>
+ * Expectation:
+ * maxMirror([1, 2, 3, 8, 9, 3, 2, 1])  → 3
+ * maxMirror([1, 2, 1, 4])              → 3
+ * maxMirror([7, 1, 2, 9, 7, 2, 1])     → 2
+ * Test:
  */
 public class MaxMirror {
+    public static int[] arr;
+
+    public static void main(String[] args) {
+        maxMirror(new int[]{1, 2, 3, 8, 9, 3, 2, 1});
+        maxMirror(new int[]{1, 2, 1, 4});
+        maxMirror(new int[]{7, 1, 2, 9, 7, 2, 1});
+    }
+
+    private static int maxMirror(int[] arr) {
+        arr = arr;
+
+        //
+        return -1;
+    }
+
 }
