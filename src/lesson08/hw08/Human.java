0f366881fc63819157321de008a9a615d393a4df
enextus <enextus@gmail.com>
2020-11-15 19:08:22 +0100

    Human ref

--- src/lesson08/hw08/Human.java
+++ src/lesson08/hw08/Human.java
@@ -9,18 +9,30 @@ import java.util.Scanner;
  */
 public class Human {
     public String name;
-    public String dateOfBirth;
+    public LocalDate dateOfBirth;
     public int fullAge;
 
-    public Human(String name, String dateOfBirth) {
+    public Human(String name, LocalDate dateOfBirth) {
         this.name = name;
         this.dateOfBirth = dateOfBirth;
-        this.fullAge = getFullAge(LocalDate.now());
+        this.fullAge = getFullAge();
     }
 
-    private int getFullAge(LocalDate now) {
-        return 0;
+    private int getFullAge() {
+        LocalDate curDate = LocalDate.now();
+        return Period.between(dateOfBirth, curDate).getYears();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Please enter date of birth in YYYY-MM-DD: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+
+        LocalDate dob = LocalDate.parse(input);
+
+        Human humanOne = new Human("Adam", dob);
+
+        System.out.println("Age is:" + humanOne.getFullAge());
     }
 }
-
-
