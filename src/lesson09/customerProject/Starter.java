60fc4f309386f85f3f77e1c8856184b043495f6e
enextus <enextus@gmail.com>
2020-11-16 21:07:54 +0100

    ref

--- src/lesson09/customerProject/Starter.java
+++ src/lesson09/customerProject/Starter.java
@@ -16,16 +16,25 @@ public class Starter {
         Cocktail disco3 = new Cocktail("Диско", cola, energiser);
         System.out.println(disco1);
 
-        System.out.println(disco1.equals(disco1));
+/*        System.out.println(disco1.equals(disco1));
         System.out.println(disco1.equals(disco2));
-        System.out.println(disco2.equals(disco3));
+        System.out.println(disco2.equals(disco3));*/
 
         Cocktail disco11 = new Cocktail("Диско11", cola, energiser);
         Cocktail disco22 = new Cocktail("Диско", cola, energiser);
         Cocktail disco33 = new Cocktail("Диско33", cola, energiser);
 
-        System.out.println(disco11.equals(disco11));
+/*        System.out.println(disco11.equals(disco11));
         System.out.println(disco1.equals(disco22));
-        System.out.println(disco22.equals(disco33));
+        System.out.println(disco22.equals(disco33));*/
+
+        int[] arr = new int[]{0, 9};
+
+        for (int i : arr) {
+            System.out.println(disco11.hashCode());
+            System.out.println(disco22.hashCode());
+            System.out.println(disco33.hashCode());
+            System.out.println();
+        }
     }
 }
