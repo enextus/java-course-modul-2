1308a32f3404e5e8e961616d00268f5d6b4fbc95
enextus <enextus@gmail.com>
2020-11-16 16:40:51 +0100

    ref

--- src/lesson08/hw08/DaysToTheEndOfTheYear.java
+++ src/lesson08/hw08/DaysToTheEndOfTheYear.java
@@ -3,10 +3,7 @@ package lesson08.hw08;
 import java.util.Scanner;
 
 /**
- * Program for determining the day number days left until the end of this year.
- * 1804 1904 2004
- * 1808 1908 2008
- * 1812 1912 2012
+ * Program for determining the day number days left until the end of the year.
  */
 public class DaysToTheEndOfTheYear {
     public static Scanner sc = new Scanner(System.in);
@@ -28,14 +25,16 @@ public class DaysToTheEndOfTheYear {
 
     private static int getNumberOfDays(int year, int month, int day) {
         if (checkLeapYear(year)) leapDay = 1;
+
         if (month < 12) {
             for (int i = month; i < numberОfDaysInMonths.length; i++) daysAfter += numberОfDaysInMonths[i];
             daysAfter += numberОfDaysInMonths[month - 1] - day;
         } else daysAfter = numberОfDaysInMonths[month - 1] - day;
+
         return leapDay + daysAfter;
     }
 
     private static boolean checkLeapYear(int a) {
-        return ((a % 4) == 0 && a % 100 != 0) || (a % 400) == 0;
+        return (a % 4 == 0 && a % 100 != 0) || (a % 400 == 0);
     }
 }
