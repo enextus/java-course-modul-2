58e41b6022bc9208baa2c0cdaee692212f585e50
enextus <enextus@gmail.com>
2020-11-18 11:59:32 +0100

    upd

--- src/lesson01/CrudArray.java
+++ src/lesson01/CrudArray.java
@@ -1,55 +1,33 @@
 package lesson01;
 
+/**
+ * lesson01.CrudArray
+ */
 public class CrudArray {
-
-    /**
-     * lesson01.CrudArray
-     */
-
-    public static void main(String[] args) {
-
-        int[] arr1 = new int[]{5, 3, 1, 9, 7};
-        printArr(arr1);
-
-        double[] arr2 = new double[]{3.14, 5.34, 10.4, 15.8, 19.6};
-        printArr(arr2);
-    }
-
     public static void printArr() {
-
         System.out.println("1. Вы вызвали пустой метод");
     }
 
     public static void printArr(String[] str) {
-
         System.out.println("2. Вы вызвали пустой метод");
     }
 
-    public static void printArr(String[] input, int size) {
-
-    }
-
     public static void printArr(double[] arr) {
-
         System.out.println("Вывод на экран double массива");
-
-        for (double element : arr) {
-
-            System.out.print(element + ", ");
-        }
-
+        for (double element : arr) System.out.print(element + ", ");
         System.out.println();
     }
 
     public static void printArr(int[] arr) {
-
         System.out.println("Вывод на экран int массива");
-
-        for (int digit : arr) {
-
-            System.out.print(digit + " ");
-        }
-
+        for (int digit : arr) System.out.print(digit + " ");
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{5, 3, 1, 9, 7};
+        printArr(arr1);
+        double[] arr2 = new double[]{3.14, 5.34, 10.4, 15.8, 19.6};
+        printArr(arr2);
     }
 }
