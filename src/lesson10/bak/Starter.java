face372a722d6ceb5b5128af033148571fac129b
enextus <enextus@gmail.com>
2020-11-18 01:51:24 +0100

    Gender upd

--- src/lesson10/bak/Starter.java
+++ src/lesson10/bak/Starter.java
@@ -8,38 +8,41 @@ import static lesson10.bak.Gender.MALE;
  * class Starter / App
  */
 public class Starter {
+    public static Gender male;
+    public static Gender female;
+    public static Gender diverse;
+    public static Alcohol drink;
+    public static Person person1;
+
+
     public static void main(String[] args) {
-        Gender in1 = MALE;
-        Gender in2 = Gender.FEMALE;
-        Gender in3 = Gender.DIVERSE;
+        male = Gender.MALE;
+        female = Gender.FEMALE;
+        diverse = Gender.DIVERSE;
 
-        System.out.println(in1);
-        System.out.println(in1.name());
-        System.out.println("Gender.valueOf(\"MALE\"): " + Gender.valueOf("MALE"));
-        System.out.println(in1.getGender());
-        System.out.println(in1.ordinal());
-        System.out.println();
-/*        System.out.println(in2);
-        System.out.println(in2.name());
-        System.out.println("Gender.valueOf(\"FEMALE\"): " + Gender.valueOf("FEMALE"));
-        System.out.println(in2.getGender());
-        System.out.println(in2.ordinal());
-        System.out.println();
-        System.out.println(in3);
-        System.out.println(in3.name());
-        System.out.println("Gender.valueOf(\"DIVERSE\"): " + Gender.valueOf("DIVERSE"));
-        System.out.println(in3.getGender());
-        System.out.println(in3.ordinal());
-        System.out.println();*/
-
-        Alcohol drink;
         drink = new Alcohol("\"соточка\"", 40.0, 100);
         System.out.println(drink);
 
-        Person person1;
-        person1 = new Person("Adam", 95.0, 190.0, MALE, LocalDate.of(1970,12,01));
+        person1 = new Person("Adam", 95.0, 190.0, male, LocalDate.of(1970, 12, 01));
         System.out.println(person1);
 
-
+/*        System.out.println(male);
+        System.out.println(male.name());
+        System.out.println("Gender.valueOf(\"MALE\"): " + Gender.valueOf("MALE"));
+        System.out.println(male.getGender());
+        System.out.println(male.ordinal());
+        System.out.println();
+        System.out.println(female);
+        System.out.println(female.name());
+        System.out.println("Gender.valueOf(\"FEMALE\"): " + Gender.valueOf("FEMALE"));
+        System.out.println(female.getGender());
+        System.out.println(female.ordinal());
+        System.out.println();
+        System.out.println(diverse);
+        System.out.println(diverse.name());
+        System.out.println("Gender.valueOf(\"DIVERSE\"): " + Gender.valueOf("DIVERSE"));
+        System.out.println(diverse.getGender());
+        System.out.println(diverse.ordinal());
+        System.out.println();*/
     }
 }
