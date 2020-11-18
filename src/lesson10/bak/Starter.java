b64373361f27b767e1a56ae2e83760b32461a91c
enextus <enextus@gmail.com>
2020-11-18 01:56:13 +0100

    Gender upd

--- src/lesson10/bak/Starter.java
+++ src/lesson10/bak/Starter.java
@@ -9,7 +9,8 @@ public class Starter {
     public static Gender male;
     public static Gender female;
     public static Gender diverse;
-    public static Alcohol drink;
+    public static Alcohol drink01;
+    public static Alcohol drink02;
     public static Person person1;
 
     public static void main(String[] args) {
@@ -20,8 +21,11 @@ public class Starter {
         drink = new Alcohol("\"соточка\"", 40.0, 100);
         System.out.println(drink);
 
+        drink = new Alcohol("\"соточка\"", 40.0, 100);
+        System.out.println(drink);
+
         person1 = new Person("Adam", 95.0, 190.0, male,
-                              LocalDate.of(1970, 12, 01));
+                LocalDate.of(1970, 12, 01));
         System.out.println(person1);
     }
 }
