64f6714fa9fff796d41e020087241eb18c0b92a4
enextus <enextus@gmail.com>
2020-11-18 01:54:02 +0100

    Gender upd

--- src/lesson10/bak/Starter.java
+++ src/lesson10/bak/Starter.java
@@ -2,8 +2,6 @@ package lesson10.bak;
 
 import java.time.LocalDate;
 
-import static lesson10.bak.Gender.MALE;
-
 /**
  * class Starter / App
  */
@@ -14,7 +12,6 @@ public class Starter {
     public static Alcohol drink;
     public static Person person1;
 
-
     public static void main(String[] args) {
         male = Gender.MALE;
         female = Gender.FEMALE;
@@ -23,26 +20,8 @@ public class Starter {
         drink = new Alcohol("\"соточка\"", 40.0, 100);
         System.out.println(drink);
 
-        person1 = new Person("Adam", 95.0, 190.0, male, LocalDate.of(1970, 12, 01));
+        person1 = new Person("Adam", 95.0, 190.0, male,
+                              LocalDate.of(1970, 12, 01));
         System.out.println(person1);
-
-/*        System.out.println(male);
-        System.out.println(male.name());
-        System.out.println("Gender.valueOf(\"MALE\"): " + Gender.valueOf("MALE"));
-        System.out.println(male.getGender());
-        System.out.println(male.ordinal());
-        System.out.println();
-        System.out.println(female);
-        System.out.println(female.name());
-        System.out.println("Gender.valueOf(\"FEMALE\"): " + Gender.valueOf("FEMALE"));
-        System.out.println(female.getGender());
-        System.out.println(female.ordinal());
-        System.out.println();
-        System.out.println(diverse);
-        System.out.println(diverse.name());
-        System.out.println("Gender.valueOf(\"DIVERSE\"): " + Gender.valueOf("DIVERSE"));
-        System.out.println(diverse.getGender());
-        System.out.println(diverse.ordinal());
-        System.out.println();*/
     }
 }
