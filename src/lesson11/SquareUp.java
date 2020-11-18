70d2ce26c0a5de0315f296c58005bf2d02b680e1
enextus <enextus@gmail.com>
2020-11-18 13:17:40 +0100

    classwork lesson11 upd

--- src/lesson11/SquareUp.java
+++ src/lesson11/SquareUp.java
@@ -23,7 +23,8 @@ public class SquareUp {
     private static int[] squareUp(int n) {
         int[] arr = new int[n * n];
         if (n == 0) return arr;
-        for (int i = n - 1; i < arr.length; i += n) for (int j = i; j >= i - (i / n); j--) arr[j] = i - j + 1;
+        for (int i = n - 1; i < arr.length; i += n)
+            for (int j = i; j >= i - (i / n); j--) arr[j] = i - j + 1;
         return arr;
     }
 }
