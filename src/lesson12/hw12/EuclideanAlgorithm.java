81c9bfc807b216fda3f4aee317f907ed05a54c47
enextus <enextus@gmail.com>
2020-11-23 22:48:07 +0100

    Euclidean algorithm

--- src/lesson12/hw12/EuclideanAlgorithm.java
+++ src/lesson12/hw12/EuclideanAlgorithm.java
@@ -1,4 +1,23 @@
 package lesson12.hw12;
 
+/**
+ * class Euclidean algorithm for greatest common divisor (GCD)
+ */
 public class EuclideanAlgorithm {
+    public static void main(String[] args) {
+        int a = 1071;
+        int b = 462;
+        int del = 0;
+
+        if (a < b) del = b;
+        else del = a;
+
+        while (true) {
+            del--;
+            if (a % del == 0 && b % del == 0) {
+                break;
+            }
+        }
+        System.out.println(del);
+    }
 }
