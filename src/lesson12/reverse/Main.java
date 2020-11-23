ac617f365e493492aff8a49401a6b99c2eb5e9dd
enextus <enextus@gmail.com>
2020-11-23 16:59:13 +0100

    reverse upd

--- src/lesson12/reverse/Main.java
+++ src/lesson12/reverse/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         Reverse resultStr = new Reverse("Hello");
-        System.out.println(resultStr.reverse("Hello"));
+        System.out.println(resultStr.reverseNaive("Hello"));
     }
 }
--- src/lesson12/reverse/Reverse.java
+++ src/lesson12/reverse/Reverse.java
@@ -1,21 +1,18 @@
 package lesson12.reverse;
 
-import lesson12.lesson12;
-
 public class Reverse {
     String str;
 
     Reverse(String str) {
-
         this.str = str;
     }
 
-    public String reverse(String str) {
+    public String reverseNaive(String str) {
         String result = "";
+
         for (int i = str.length() - 1; i >= 0; i--) {
             result += str.charAt(i);
         }
         return result;
     }
 }
-
