1ae2a560d04c0eb8a1cf7f7c044a4030ede6247f
enextus <enextus@gmail.com>
2020-11-23 23:04:25 +0100

    Euclidean algorithm upd

--- src/lesson12/hw12/EuclideanAlgorithm.java
+++ src/lesson12/hw12/EuclideanAlgorithm.java
@@ -11,13 +11,14 @@ public class EuclideanAlgorithm {
     public static void main(String[] args) {
         int a = 1071;
         int b = 462;
-        int gcd = 0;
 
-        System.out.println("GCD = " + euclidean(a, b, gcd));
+        System.out.println("GCD = " + euclidean(a, b));
         System.out.println();
     }
 
-    private static int euclidean(int a, int b, int gcd) {
+    private static int euclidean(int a, int b) {
+        int gcd;
+
         if (a < b) gcd = b;
         else gcd = a;
 
